5e2776bfe09e93792e9c461636a68cbc30b1b123
Hajk <tunczorx@gmail.com>
2018-07-31 09:48:30 +0200

    Simple diffrence between structure and object.

--- LearningStructuresV1.0.0/Program.cs
+++ LearningStructuresV1.0.0/Program.cs
@@ -10,6 +10,18 @@ namespace LearningStructuresV1._0._0
     {
         static void Main(string[] args)
         {
+            Canine spot = new Canine("Burek", "mops");
+            Canine bob = spot;
+            bob.Name = "Szarik";
+            bob.Breed = "beagle";
+            spot.Speak();
+
+            Dog jake = new Dog("Tofik", "pudel");
+            Dog betty = jake; //diffrence between object and structure is this one. Here we created jake copy with betty parametrs. Now if we change fields like name and breed it will be changed only for betty, not for jake.
+            betty.Name = "Becia";
+            betty.Breed = "pitbul";
+            jake.Speak();
+            Console.ReadKey();
         }
     }
 }
